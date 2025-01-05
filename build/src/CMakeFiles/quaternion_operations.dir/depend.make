008e985803d68b3c0ecaeac22d13c3ac2003303e
alexshain <sashashain2000@gmail.com>
2025-01-06 00:18:04 +0700

    удалил build

--- build/src/CMakeFiles/quaternion_operations.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for quaternion_operations.
-# This may be replaced when dependencies are built.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alexandrshain/cpp/Quaternions/tests
-# Build directory: /Users/alexandrshain/cpp/Quaternions/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(QuaternionOperationsTest "/Users/alexandrshain/cpp/Quaternions/build/tests/test_quaternion_operations")
-set_tests_properties(QuaternionOperationsTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/alexandrshain/cpp/Quaternions/tests/CMakeLists.txt;22;add_test;/Users/alexandrshain/cpp/Quaternions/tests/CMakeLists.txt;0;")
